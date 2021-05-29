bcdcb0758b94b3257a3a34df28a8b7edcbdbddc1
Splinter1080 <61391824+Splinter1080@users.noreply.github.com>
2021-05-29 22:17:28 +0530

    Update PlayerCollision.cs

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
 
-	public PlayerMovement movement;     // A reference to our PlayerMovement script
+	public PlayerMovement movement;     
 
-	// This function runs when we hit another object.
-	// We get information about the collision and call it "collisionInfo".
 	void OnCollisionEnter (Collision collisionInfo)
 	{
-		// We check if the object we collided with has a tag called "Obstacle".
+		
 		if (collisionInfo.collider.tag == "Obstacle")
 		{
-			movement.enabled = false;   // Disable the players movement.
+			movement.enabled = false;   
 			FindObjectOfType<GameManager>().EndGame();
 		}
 	}
